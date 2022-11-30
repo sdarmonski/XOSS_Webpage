3431920e0c0e8c0252f4dbba6a94fad984328289
xoss <s.darmonski@uvionix.com>
2022-11-30 16:48:41 +0200

    Added latest LTE status display

--- fetch-log.js
+++ fetch-log.js
@@ -11,12 +11,15 @@ document.querySelector("#fetch-log-btn").addEventListener('click', async functio
             logging_started = true;
             document.getElementById("fetch-log-btn").textContent = "Stop Logging";
             document.querySelector("#preview-log").textContent = "Logging started. Waiting for data...";
+            document.querySelector("#lte-latest-status").textContent = "Logging started. Waiting for data...";
             intervalId = setInterval( async function()
             {
                 try
                 {
                     let text_data = await fetchLog();
                     document.querySelector("#preview-log").textContent = text_data;
+                    text_data = await fetchLTElatestStatus();
+                    document.querySelector("#lte-latest-status").textContent = text_data;
                 }
                 catch (e)
                 {
@@ -58,10 +61,25 @@ async function fetchLog()
     return text_data;
 }
 
+async function fetchLTElatestStatus()
+{
+    let response = await fetch("modem-latest-status");
+
+    if (!response.ok)
+    {
+        throw new Error("Server Error");
+    }
+
+    let text_data = await response.text();
+
+    return text_data;
+}
+
 function stopLogging()
 {
     logging_started = false;
     clearInterval(intervalId);
     document.getElementById("fetch-log-btn").textContent = "Display Log";
     document.querySelector("#preview-log").textContent = "Logging stopped.";
+    document.querySelector("#lte-latest-status").textContent = "Logging stopped.";
 }
